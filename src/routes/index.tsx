947d996265d653622ab913480caf1934e4ddc26d
Jordão Beghetto Massariol <jbmassariol@gmail.com>
2022-10-13 19:26:25 -0300

    feat: Adicionando notificações

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,12 +1,20 @@
 /* eslint-disable react/react-in-jsx-scope */
 import { NavigationContainer } from "@react-navigation/native";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import { useEffect } from "react";
 import { RootStackParamList } from "../@types/navigation";
 import { Home } from "../pages/Home";
 import { Reminder } from "../pages/Reminder";
+import NotifService from "../services/NotifService";
+
+const notifService = new NotifService();
 
 export const Routes = () => {
   const Stack = createNativeStackNavigator<RootStackParamList>();
+
+  useEffect(() => {
+    notifService.localNotif();
+  }, []);
 
   return (
     <>
